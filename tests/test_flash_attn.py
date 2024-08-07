f5d14f0a856abbf749bcdf46f549d6867eee6ee1
Michael Melesse <micmelesse@gmail.com>
2024-08-07 12:16:01 -0500

    check for paged_kv_block_size in test_flash_attn_varlen_causal

--- tests/test_flash_attn.py
+++ tests/test_flash_attn.py
@@ -1708,6 +1708,9 @@ def test_flash_attn_varlen_causal(
     if is_amd():
         if local == True:
             pytest.skip("local sliding window attention not supported on AMD yet")
+
+        if paged_kv_block_size is not None:
+            pytest.skip("paged attention not supported on AMD yet")
         
         # skip all cases where seqlen_q, seqlen_k, or d are not powers of 2
         if not (is_power_of_2(seqlen_q) and is_power_of_2(seqlen_k) and is_power_of_2(d)):
