179d8281a6d3cde586c915d25f98b5d17cab7540
Kieran Hunt <kph@hotmail.ca>
2024-09-01 19:23:48 -0400

    hope this works!

--- csrc/fused_dense_lib/setup.py
+++ csrc/fused_dense_lib/setup.py
@@ -23,19 +23,56 @@ def append_nvcc_threads(nvcc_extra_args):
     return nvcc_extra_args
 
 
+def rename_cpp_to_hip(cpp_files):
+    for entry in cpp_files:
+        shutil.copy(entry, os.path.splitext(entry)[0] + ".hip")
+
+# Defining a function to validate the GPU architectures and update them if necessary
+def validate_and_update_archs(archs):
+    # List of allowed architectures
+    allowed_archs = ["native", "gfx90a", "gfx940", "gfx941", "gfx942", "gfx1100", "gfx1101"]
+
+    # Validate if each element in archs is in allowed_archs
+    assert all(
+        arch in allowed_archs for arch in archs
+    ), f"One of GPU archs of {archs} is invalid or not supported by Flash-Attention"
+
+def build_for_rocm():
+    """build for ROCm platform"""
+
+    archs = os.getenv("GPU_ARCHS", "native").split(";")
+    validate_and_update_archs(archs)
+    cc_flag = [f"--offload-arch={arch}" for arch in archs]
+
+    if int(os.environ.get("FLASH_ATTENTION_INTERNAL_USE_RTN", 0)):
+        print("RTN IS USED")
+        cc_flag.append("-DUSE_RTN_BF16_CONVERT")
+    else:
+        print("RTZ IS USED")
+
+    fa_sources = ["fused_dense_hip.cpp", "fused_dense.cpp"] #+ glob.glob("src/*.cpp")
+
+    rename_cpp_to_hip(fa_sources)
+
+    ext_modules.append(
+        CUDAExtension(
+            'fused_dense_lib', [
+                'fused_dense_hip.hip',
+                'fused_dense.hip'
+            ], #+ glob.glob("src/*.hip"),
+            extra_compile_args={"cxx": ["-O3", "-std=c++17", "-DNDEBUG"],
+                                'nvcc': [
+                                    '-O3', "-std=c++17", "-DNDEBUG"
+                                ] + cc_flag
+                               }
+        )
+    )
+
+build_for_rocm()
+
 setup(
     name='fused_dense_lib',
-    ext_modules=[
-        CUDAExtension(
-            name='fused_dense_lib',
-            sources=['fused_dense.cpp', 'fused_dense_cuda.cu'],
-            extra_compile_args={
-                               'cxx': ['-O3',],
-                               'nvcc': append_nvcc_threads(['-O3'])
-                               }
-            )
-    ],
-    cmdclass={
-        'build_ext': BuildExtension
-})
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": BuildExtension} if ext_modules else {},
+)
 
