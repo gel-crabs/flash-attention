16a795b4ada549475b738cd17262336bec05054c
Kieran Hunt <kph@hotmail.ca>
2024-09-06 12:11:21 -0400

    Update fwd_device_gemm_template.hpp

--- csrc/flash_attn_rocm/src/fwd_device_gemm_template.hpp
+++ csrc/flash_attn_rocm/src/fwd_device_gemm_template.hpp
@@ -541,13 +541,26 @@ using DeviceGemmBatchedGQA = device_op::DeviceGroupedQueryAttentionForward_Wmma<
     typename DeviceGemmTraits::VElementOp,
     typename DeviceGemmTraits::OutElementOp, DeviceGemmTraits::kGemmSpec,
     DeviceGemmTraits::kTensorSpecQ, DeviceGemmTraits::kTensorSpecK,
-    DeviceGemmTraits::kTensorSpecV, DeviceGemmTraits::kTensorSpecOut, 1, QueryGroupNumber, 512,
+    DeviceGemmTraits::kTensorSpecV, DeviceGemmTraits::kTensorSpecOut,
+    1, // NumPrefetch
+    512, // BlockSize
     //      Gemm 0
-    256, 128, 64, 8, 8,
+    256, // MPerBlock
+    128, // LPerBlock
+    64, // KPerBlock
+    8, // AK1
+    8, // BK1
     //      Gemm 1
-    64, 64, 8, 16, 16, 16,
+    64, // NPerBlock
+    64, // LTilePerBlock
+    8, // L1
+    16, // MPerWmma
+    16, // LPerWmma
+    16, // NPerWmma
     // Per repeat = wave_m = wave_num, wave_n = 1
-    1, 8, 4,
+    1, // MRepeat
+    8, // LRepeat
+    4, // NRepeat
     // ABlockTransfer MK -> K0 M K1
     device_gemm_trait::S<2, 256, 1>, device_gemm_trait::S<1, 0, 2>,
     device_gemm_trait::S<1, 0, 2>, 2, 8, 8, true,
@@ -558,7 +571,7 @@ using DeviceGemmBatchedGQA = device_op::DeviceGroupedQueryAttentionForward_Wmma<
     device_gemm_trait::S<2, 32, 8>, device_gemm_trait::S<0, 2, 1>,
     device_gemm_trait::S<0, 2, 1>, 1, 1, 1, false,
     // CShuffleBlockTransfer MN
-    1, 1, device_gemm_trait::S<1, 256, 1, 2>, 8,
+    1, 1, device_gemm_trait::S<1, 256, 1, 1>, 8,
     DeviceGemmTraits::kMaskingSpec>;
 } // namespace wmma
 #endif
