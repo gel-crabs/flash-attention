c8f30690529047cc5ccc50dd73164c4ed1f6e6b2
Kieran Hunt <kph@hotmail.ca>
2024-09-06 16:16:45 -0400

    Update flash_runner.hpp

--- csrc/flash_attn_rocm/src/flash_runner.hpp
+++ csrc/flash_attn_rocm/src/flash_runner.hpp
@@ -104,14 +104,12 @@ public:
   template <typename FlashParams>
   void Run(FlashParams &params, hipStream_t &stream) {
     BOOL_SWITCH((params.h_kv == 1), kIsMQA, [&] {
-      BF16_SWITCH(params.is_bf16, [&] {
         BOOL_SWITCH(params.is_mnko_padding, kIsPadding, [&] {
           BOOL_SWITCH(params.is_causal, kIsCausal, [&] {
             this->template run_<FlashParams, kIsMQA, T, kIsPadding, kIsCausal>(
                 params, stream);
           });
         });
-      });
     });
   }
 
