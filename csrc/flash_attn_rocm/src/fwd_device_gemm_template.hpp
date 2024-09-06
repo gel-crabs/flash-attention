9c117104a2e69a34927a79af1bdd277eb46cd3d2
Kieran Hunt <kph@hotmail.ca>
2024-09-06 11:39:17 -0400

    oh, this definitely isn't gonna work

--- csrc/flash_attn_rocm/src/fwd_device_gemm_template.hpp
+++ csrc/flash_attn_rocm/src/fwd_device_gemm_template.hpp
@@ -518,7 +518,7 @@ using DeviceGemmBatchedMQA = device_op::DeviceMultiQueryAttentionForward_Wmma<
     device_gemm_trait::S<1, 0, 2>, 2, 8, 8, true,
     // B1BlockTransfer NL -> L0 N L1
     device_gemm_trait::S<2, 32, 8>, device_gemm_trait::S<0, 2, 1>,
-    device_gemm_trait::S<0, 2, 1>, 1, 1, 1, false,
+    device_gemm_trait::S<0, 2, 1>, 1, 0.5, 1, false,
     // CShuffleBlockTransfer MN
     1, 1, device_gemm_trait::S<1, 256, 1, 2>, 8,
     DeviceGemmTraits::kMaskingSpec>;
@@ -556,7 +556,7 @@ using DeviceGemmBatchedGQA = device_op::DeviceGroupedQueryAttentionForward_Wmma<
     device_gemm_trait::S<1, 0, 2>, 2, 8, 8, true,
     // B1BlockTransfer NL -> L0 N L1
     device_gemm_trait::S<2, 32, 8>, device_gemm_trait::S<0, 2, 1>,
-    device_gemm_trait::S<0, 2, 1>, 1, 1, 1, false,
+    device_gemm_trait::S<0, 2, 1>, 1, 0.5, 1, false,
     // CShuffleBlockTransfer MN
     1, 1, device_gemm_trait::S<1, 256, 1, 2>, 8,
     DeviceGemmTraits::kMaskingSpec>;
