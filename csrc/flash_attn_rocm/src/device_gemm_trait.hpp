bdd6b9184f4672aff810d5c84cd60357c1c3078c
Kieran Hunt <kph@hotmail.ca>
2024-09-06 17:32:34 -0400

    perhaps this could work

--- csrc/flash_attn_rocm/src/device_gemm_trait.hpp
+++ csrc/flash_attn_rocm/src/device_gemm_trait.hpp
@@ -50,7 +50,7 @@ using Int32 = int;
 using Int16 = unsigned short;
 using Int8 = uint8_t;
 using Float32 = float;
-using BFloat16 = unsigned short;
+using BFloat16 = ck::bhalf_t;
 using Float16 = ck::half_t;
 
 using PassThrough = ck::tensor_operation::element_wise::PassThrough;
