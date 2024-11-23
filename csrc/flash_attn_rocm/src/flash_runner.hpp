8861d881503eb937160db2d2bafab8eb590a63b5
Kieran Hunt <kph@hotmail.ca>
2024-11-23 15:26:18 -0500

    Update flash_runner.hpp

--- csrc/flash_attn_rocm/src/flash_runner.hpp
+++ csrc/flash_attn_rocm/src/flash_runner.hpp
@@ -54,14 +54,15 @@ private:
             bool kIsCausal>
   void run_(FlashParams &params, hipStream_t &stream);
 
+  using DeviceGemmTraits =
+        device_gemm_trait::Forward<T, kGemmSpec, kMaskingSpec, kHeadDim>;
+
   template <typename FlashFwdParams,
             template <typename> typename DeviceGemmTemplate, typename T,
             device_gemm_trait::GemmSpec kGemmSpec,
-            device_gemm_trait::MaskingSpec kMaskingSpec, Index KHeadDim = kHeadDim, bool kIsDeterministic>
+            device_gemm_trait::MaskingSpec kMaskingSpec, bool kIsDeterministic>
   void run_fwd_(FlashFwdParams &params, hipStream_t &stream) {
     // input, output, gemm, dropout, cshuffle, masking specialization,
-    using DeviceGemmTraits =
-        device_gemm_trait::Forward<T, kGemmSpec, kMaskingSpec, KHeadDim>;
     using Invoker = fwd_device_gemm::wmma::DeviceGemmInvoker<DeviceGemmTemplate,
                                                              DeviceGemmTraits>;
     Invoker(params, stream);
