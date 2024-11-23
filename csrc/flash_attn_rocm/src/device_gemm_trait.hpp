c4920b770766ae17485a3db1deda616ff4513e70
Kieran Hunt <kph@hotmail.ca>
2024-11-22 20:17:14 -0500

    Update device_gemm_trait.hpp

--- csrc/flash_attn_rocm/src/device_gemm_trait.hpp
+++ csrc/flash_attn_rocm/src/device_gemm_trait.hpp
@@ -89,7 +89,7 @@ struct Forward {
   static constexpr Index kNumDimN = 1;
   static constexpr Index kNumDimK = 1;
   static constexpr Index kNumDimO = 1;
-  static constexpr auto QueryGroupNumber = FlashFwdBatchedParams::d;
+  using QueryGroupNumber = FlashFwdBatchedParams::d;
 
   static constexpr auto kGemmSpec = kGemmSpec_;
 
