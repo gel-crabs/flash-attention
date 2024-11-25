5672ba8ad85f68b79a08c38a9ef65e087cd16799
Kieran Hunt <kph@hotmail.ca>
2024-11-25 10:21:28 -0500

    holy shit

--- csrc/flash_attn_rocm/src/fwd_device_gemm_invoker.hpp
+++ csrc/flash_attn_rocm/src/fwd_device_gemm_invoker.hpp
@@ -61,10 +61,10 @@ public:
         true,
         true);
 
-    //if (!gemm_ptr->IsSupportedArgument(argument)) {
-    //  throw std::runtime_error(gemm_ptr->GetTypeString() +
-    //                           " does not support this problem");
-    //}
+    if (!gemm_ptr->IsSupportedArgument(argument)) {
+      throw std::runtime_error(gemm_ptr->GetTypeString() +
+                               " does not support this problem");
+    }
     auto time_kernel = get_env_("FLASH_ATTENTION_INTERNAL_ENABLE_TIME_KERNEL");
     auto avg_time = invoker.Run(argument, StreamConfig{stream, time_kernel});
 
