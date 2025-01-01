811c6d50068f48d11d0477ddf3e3bafd1f9625bf
Kieran Hunt <kph@hotmail.ca>
2025-01-01 17:58:36 -0500

    sfdsdf

--- csrc/flash_attn_rocm/src/fwd_device_gemm_invoker.hpp
+++ csrc/flash_attn_rocm/src/fwd_device_gemm_invoker.hpp
@@ -153,21 +153,14 @@ public:
   // constructor for batched gemm
   explicit DeviceGemmInvoker(FlashFwdBatchedParams &params,
                              hipStream_t &stream) {
+    DeviceMem a_device_buf(sizeof(ADataType) * params.q_ptr.mDesc.GetElementSpaceSize());
+    DeviceMem b0_device_buf(sizeof(B0DataType) * params.k_ptr.mDesc.GetElementSpaceSize());
+    DeviceMem b1_device_buf(sizeof(B1DataType) * params.v_ptr.mDesc.GetElementSpaceSize());
+    DeviceMem c_device_buf(sizeof(CDataType) * params.out_ptr.mDesc.GetElementSpaceSize());
 
-    using a_gs_ms_ks = Tensor<ADataType>(params.q_lengths, params.q_strides);
-    using b0_gs_ns_ks = Tensor<B0DataType>(params.k_lengths, params.k_strides);
-    using b1_gs_os_ns = Tensor<B1DataType> (params.v_lengths, params.v_strides);
-    using c_gs_ms_os_host_result = Tensor<CDataType>(params.out_lengths, params.out_strides);
-    using c_gs_ms_os_device_result = Tensor<CDataType>(params.out_lengths, params.out_strides);
-
-    DeviceMem a_device_buf(sizeof(ADataType) * a_gs_ms_ks.mDesc.GetElementSpaceSize());
-    DeviceMem b0_device_buf(sizeof(B0DataType) * b0_gs_ns_ks.mDesc.GetElementSpaceSize());
-    DeviceMem b1_device_buf(sizeof(B1DataType) * b1_gs_os_ns.mDesc.GetElementSpaceSize());
-    DeviceMem c_device_buf(sizeof(CDataType) * c_gs_ms_os_device_result.mDesc.GetElementSpaceSize());
-
-    a_device_buf.ToDevice(a_gs_ms_ks.mData.data());
-    b0_device_buf.ToDevice(b0_gs_ns_ks.mData.data());
-    b1_device_buf.ToDevice(b1_gs_os_ns.mData.data());
+    a_device_buf.ToDevice(params.q_ptr.mData.data());
+    b0_device_buf.ToDevice(params.k_ptr.mData.data());
+    b1_device_buf.ToDevice(params.v_ptr.mData.data());
 
     auto gemm_ptr = std::make_unique<Gemm>();
     auto invoker = gemm_ptr->MakeInvoker();
